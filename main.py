e2b76151621d1b29e8422abbde9391d45ff65556
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-04-18 14:19:19 -0300

    Criação das tabelas cliente, produtos, pedidos e detalhes pedido

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
+"""Este arquivo contém tabelas para um banco de dados de gerenciamento de pedidos"""
 import sqlite3
 
 conn = sqlite3.connect('empresa_abc.db')
 c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS Clientes (
+                ID_Cliente INTEGER PRIMARY KEY,
+                Nome TEXT,
+                Pais TEXT
+             )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS Produtos (
+                ID_Produto INTEGER PRIMARY KEY,
+                Nome_Produto TEXT,
+                Categoria TEXT,
+                Preco REAL
+             )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS Pedidos (
+                ID_Pedido INTEGER PRIMARY KEY,
+                ID_Cliente INTEGER,
+                Data_Pedido TEXT,
+                FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente)
+             )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS Detalhes_Pedido (
+                ID_Pedido INTEGER,
+                ID_Produto INTEGER,
+                Quantidade INTEGER,
+                FOREIGN KEY (ID_Pedido) REFERENCES Pedidos(ID_Pedido),
+                FOREIGN KEY (ID_Produto) REFERENCES Produtos(ID_Produto)
+             )''')
+
+conn.commit()
+conn.close()
